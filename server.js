f4a18981878f7f9d8f0e57aab1e75ed0ec5cd4a0
Jithin <153214952+jiithin@users.noreply.github.com>
2024-07-01 00:13:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({origin: ["http://localhost:5173"], credentials: true,}));
+app.use(cors({origin: ["https://inventory-app-reactjithi.netlify.app"], credentials: true,}));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
